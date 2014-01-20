2b4b3e3d7a38513dd0e31f35143609cb1bf9fbca
alstroemeria <alstroemeri@gmail.com>
2014-01-19 23:28:11 -0500

    removed old migration:

--- db/migrate/20140120005911_add_box_office_to_movie.rb
+++
@@ -1,8 +0,0 @@
-class AddBoxOfficeToMovie < ActiveRecord::Migration
-  def change
-    add_column :movies, :box_office, :boolean
-    add_column :movies, :in_theaters, :boolean
-    add_column :movies, :opening, :boolean
-    add_column :movies, :upcoming, :boolean
-  end
-end
